5896aed75e81fea42c2c2dd8dcee5c6cf0aabf8b
Sean Chuang <chunghsiang.chuang@smartnews.com>
2020-05-19 06:48:12 +0800

    Only save user list

--- unsupervised_embedding/get_s2v_emb.py
+++ unsupervised_embedding/get_s2v_emb.py
@@ -7,7 +7,14 @@ import argparse
 
 model = sent2vec.Sent2vecModel()
 
-def fetch_embedding(model_file, data_prefix, output_file):
+def get_user_list(user_list_file):
+    user_set = set()
+    with open(user_list_file, 'r') as in_f:
+        for line in in_f:
+            user_set.add(line.strip())
+    return user_set
+
+def fetch_embedding(model_file, data_prefix, user_list, output_file):
     model.load_model(model_file)
 
     file_list = glob.glob(data_prefix + '*')
@@ -17,15 +24,20 @@ def fetch_embedding(model_file, data_prefix, output_file):
                 for line in in_f:
                     tmp = line.strip().split(' ', 1)
                     user_id = tmp[0]
-                    vector = model.embed_sentence(tmp[1])[0]
-                    vector_str = [str(i) + ':' + f'{v:.4f}' for i,v in enumerate(vector)]
-                    out_f.write(user_id + '\t' + ' '.join(vector_str) + '\n')
+                    if user_id in user_list:
+                        vector = model.embed_sentence(tmp[1])[0]
+                        vector_str = [str(i) + ':' + f'{v:.4f}' for i,v in enumerate(vector)]
+                        out_f.write(user_id + '\t' + ' '.join(vector_str) + '\n')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("python3 s2v_fetch_vector.py")
     parser.add_argument("model", type=str)
     parser.add_argument("data_prefix", type=str, help="User histroy events file prefix")
+    parser.add_argument("user_list", type=str, help="Avaliable user in test data")
     parser.add_argument("embed_path", type=str, help="Output embedding path")
     args = parser.parse_args()
-    fetch_embedding(args.model, args.data_prefix, args.embed_path)
+    print('0. Get user list')
+    user_list = get_user_list(args.user_list)
+    print('1. Get user embedding')
+    fetch_embedding(args.model, args.data_prefix, user_list, args.embed_path)
